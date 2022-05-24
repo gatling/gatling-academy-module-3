be07a460783fbab1f5a1bdae9dff83343f3c1e1d
James Willett <james.willett@sap.com>
2022-05-24 11:42:10 +0100

    Adding code for video A7

--- src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
+++ src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
@@ -1,101 +1,82 @@
 package gatlingdemostoreapi;
 
-import java.time.Duration;
 import java.util.*;
 
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
-import io.gatling.javaapi.jdbc.*;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
-import static io.gatling.javaapi.jdbc.JdbcDsl.*;
 
 public class DemostoreApiSimulation extends Simulation {
 
   private HttpProtocolBuilder httpProtocol = http
     .baseUrl("https://demostore.gatling.io")
-    .inferHtmlResources(AllowList(), DenyList())
-    .acceptHeader("*/*")
-    .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("PostmanRuntime/7.26.8");
+    .header("Cache-Control", "no-cache")
+    .contentTypeHeader("application/json")
+    .acceptHeader("application/json");
+
   
-  private Map<CharSequence, String> headers_0 = Map.of("Cache-Control", "no-cache");
-  
-  private Map<CharSequence, String> headers_3 = Map.ofEntries(
-    Map.entry("Cache-Control", "no-cache"),
-    Map.entry("Content-Type", "application/json")
-  );
-  
-  private Map<CharSequence, String> headers_4 = Map.ofEntries(
-    Map.entry("Cache-Control", "no-cache"),
-    Map.entry("Content-Type", "application/json"),
+  private Map<CharSequence, String> authorizationHeaders = Map.ofEntries(
     Map.entry("authorization", "Bearer #{jwt}")
   );
 
 
   private ScenarioBuilder scn = scenario("DemostoreApiSimulation")
     .exec(
-      http("request_0")
+      http("List categories")
         .get("/api/category")
-        .headers(headers_0)
-        .check(bodyBytes().is(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0000_response.json")))
     )
     .pause(2)
     .exec(
-      http("request_1")
+      http("List products")
         .get("/api/product?category=7")
-        .headers(headers_0)
-        .check(bodyBytes().is(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0001_response.json")))
     )
     .pause(2)
     .exec(
-      http("request_2")
+      http("Get product")
         .get("/api/product/34")
-        .headers(headers_0)
-        .check(bodyBytes().is(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0002_response.json")))
     )
     .pause(2)
     .exec(
-      http("request_3")
+      http("Authenticate")
         .post("/api/authenticate")
-        .headers(headers_3)
-        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0003_request.json"))
+        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/authenticate-admin.json"))
         .check(jsonPath("$.token").saveAs("jwt"))
     )
     .pause(2)
     .exec(
-      http("request_4")
+      http("Update product")
         .put("/api/product/34")
-        .headers(headers_4)
-        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0004_request.json")))
+        .headers(authorizationHeaders)
+        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/update-product.json")))
     .pause(2)
     .exec(
-      http("request_5")
+      http("Create product 1")
         .post("/api/product")
-        .headers(headers_4)
-        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0005_request.json"))
+        .headers(authorizationHeaders)
+        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/create-product1.json"))
     )
     .pause(2)
     .exec(
-      http("request_6")
+      http("Create product 2")
         .post("/api/product")
-        .headers(headers_4)
-        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0006_request.json"))
+        .headers(authorizationHeaders)
+        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/create-product2.json"))
     )
     .pause(2)
     .exec(
-      http("request_7")
+      http("Create product 3")
         .post("/api/product")
-        .headers(headers_4)
-        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0007_request.json"))
+        .headers(authorizationHeaders)
+        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/create-product3.json"))
     )
     .pause(2)
     .exec(
-      http("request_8")
+      http("Update category")
         .put("/api/category/7")
-        .headers(headers_4)
-        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/0008_request.json"))
+        .headers(authorizationHeaders)
+        .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/update-category.json"))
     );
 
   {
