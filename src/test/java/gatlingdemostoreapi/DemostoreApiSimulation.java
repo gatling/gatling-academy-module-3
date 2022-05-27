e00767515b03f055d589c86c625f7f12d28f2b51
James Willett <james.willett@sap.com>
2022-05-27 09:48:20 +0100

    Adding code for video B8

--- src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
+++ src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
@@ -11,27 +11,27 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 public class DemostoreApiSimulation extends Simulation {
 
   private HttpProtocolBuilder httpProtocol = http
-    .baseUrl("https://demostore.gatling.io")
-    .header("Cache-Control", "no-cache")
-    .contentTypeHeader("application/json")
-    .acceptHeader("application/json");
+          .baseUrl("https://demostore.gatling.io")
+          .header("Cache-Control", "no-cache")
+          .contentTypeHeader("application/json")
+          .acceptHeader("application/json");
 
-  
+
   private static Map<CharSequence, String> authorizationHeaders = Map.ofEntries(
-    Map.entry("authorization", "Bearer #{jwt}")
+          Map.entry("authorization", "Bearer #{jwt}")
   );
 
   private static ChainBuilder initSession = exec(session -> session.set("authenticated", false));
 
   private static class Authentication {
     private static ChainBuilder authenticate =
-      doIf(session -> !session.getBoolean("authenticated")).then(
-              exec(http("Authenticate")
-                      .post("/api/authenticate")
-                      .body(StringBody("{\"username\": \"admin\",\"password\": \"admin\"}"))
-                      .check(status().is(200))
-                      .check(jmesPath("token").saveAs("jwt")))
-                      .exec(session -> session.set("authenticated", true)));
+            doIf(session -> !session.getBoolean("authenticated")).then(
+                    exec(http("Authenticate")
+                            .post("/api/authenticate")
+                            .body(StringBody("{\"username\": \"admin\",\"password\": \"admin\"}"))
+                            .check(status().is(200))
+                            .check(jmesPath("token").saveAs("jwt")))
+                            .exec(session -> session.set("authenticated", true)));
   }
 
   private static class Categories {
@@ -40,18 +40,18 @@ public class DemostoreApiSimulation extends Simulation {
             csv("data/categories.csv").random();
 
     private static ChainBuilder list =
-        exec(http("List categories")
-          .get("/api/category")
-          .check(jmesPath("[? id == `6`].name").ofList().is(List.of("For Her"))));
+            exec(http("List categories")
+                    .get("/api/category")
+                    .check(jmesPath("[? id == `6`].name").ofList().is(List.of("For Her"))));
 
     private static ChainBuilder update =
-        feed(categoriesFeeder)
-        .exec(Authentication.authenticate)
-        .exec(http("Update category")
-          .put("/api/category/#{categoryId}")
-          .headers(authorizationHeaders)
-          .body(StringBody("{\"name\": \"#{categoryName}\"}"))
-          .check(jmesPath("name").isEL("#{categoryName}")));
+            feed(categoriesFeeder)
+                    .exec(Authentication.authenticate)
+                    .exec(http("Update category")
+                            .put("/api/category/#{categoryId}")
+                            .headers(authorizationHeaders)
+                            .body(StringBody("{\"name\": \"#{categoryName}\"}"))
+                            .check(jmesPath("name").isEL("#{categoryName}")));
   }
 
   private static class Products {
@@ -60,48 +60,65 @@ public class DemostoreApiSimulation extends Simulation {
             csv("data/products.csv").circular();
 
     private static ChainBuilder list =
-        exec(http("List products")
-          .get("/api/product?category=7")
-          .check(jmesPath("[? categoryId != '7']").ofList().is(Collections.emptyList())));
+            exec(http("List products")
+                    .get("/api/product?category=7")
+                    .check(jmesPath("[? categoryId != '7']").ofList().is(Collections.emptyList()))
+                    .check(jmesPath("[*].id").ofList().saveAs("allProductIds")));
 
     private static ChainBuilder get =
-        exec(http("Get product")
-          .get("/api/product/34")
-          .check(jmesPath("id").ofInt().is(34)));
+            exec(session -> {
+              List<Integer> allProductIds = session.getList("allProductIds");
+              return session.set("productId", allProductIds.get(new Random().nextInt(allProductIds.size())));
+            })
+              .exec(http("Get product")
+                            .get("/api/product/#{productId}")
+                            .check(jmesPath("id").ofInt().isEL("#{productId}"))
+                            .check(jmesPath("@").ofMap().saveAs("product")));
+
 
     private static ChainBuilder update =
-       exec(Authentication.authenticate)
-       .exec(http("Update product")
-          .put("/api/product/34")
-          .headers(authorizationHeaders)
-          .body(RawFileBody("gatlingdemostoreapi/demostoreapisimulation/update-product.json"))
-          .check(jmesPath("price").is("15.99")));
+            exec(Authentication.authenticate)
+              .exec( session -> {
+                  Map<String, Object> product = session.getMap("product");
+                  return session
+                          .set("productCategoryId", product.get("categoryId"))
+                          .set("productName", product.get("name"))
+                          .set("productDescription", product.get("description"))
+                          .set("productImage", product.get("image"))
+                          .set("productPrice", product.get("price"))
+                          .set("productId", product.get("id"));
+              })
+                    .exec(http("Update product #{productName}")
+                            .put("/api/product/#{productId}")
+                            .headers(authorizationHeaders)
+                            .body(ElFileBody("gatlingdemostoreapi/demostoreapisimulation/create-product.json"))
+                            .check(jmesPath("price").isEL("#{productPrice}")));
 
     private static ChainBuilder create =
-       exec(Authentication.authenticate)
-         .feed(productsFeeder)
-         .exec(http("Create product #{productName}")
-                 .post("/api/product")
-                 .headers(authorizationHeaders)
-                 .body(ElFileBody("gatlingdemostoreapi/demostoreapisimulation/create-product.json")));
+            exec(Authentication.authenticate)
+                    .feed(productsFeeder)
+                    .exec(http("Create product #{productName}")
+                            .post("/api/product")
+                            .headers(authorizationHeaders)
+                            .body(ElFileBody("gatlingdemostoreapi/demostoreapisimulation/create-product.json")));
   }
 
 
   private ScenarioBuilder scn = scenario("DemostoreApiSimulation")
-    .exec(initSession)
-    .exec(Categories.list)
-    .pause(2)
-    .exec(Products.list)
-    .pause(2)
-    .exec(Products.get)
-    .pause(2)
-    .exec(Products.update)
-    .pause(2)
-    .repeat(3).on(exec(Products.create))
-    .pause(2)
-    .exec(Categories.update);
+          .exec(initSession)
+          .exec(Categories.list)
+          .pause(2)
+          .exec(Products.list)
+          .pause(2)
+          .exec(Products.get)
+          .pause(2)
+          .exec(Products.update)
+          .pause(2)
+          .repeat(3).on(exec(Products.create))
+          .pause(2)
+          .exec(Categories.update);
 
   {
-	  setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
+    setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
   }
 }
